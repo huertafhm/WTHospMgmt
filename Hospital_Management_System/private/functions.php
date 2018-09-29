be7d8938aa0675e9f9a691f71c834b10ce3a12b0
huertafhm <huerta.fhm@gmail.com>
2018-09-29 15:07:28 +1000

    first commit

--- Hospital_Management_System/private/functions.php
+++ Hospital_Management_System/private/functions.php
@@ -1,2 +1,29 @@
-<?php ?>
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "HospMgmt";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+function getDoctorById($id){
+    $servername = "localhost";
+    $username = "root";
+    $password = "password";
+    $dbname = "HospMgmt";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $sql = "SELECT*FROM doctor WHERE doctorId="+$id+";";
+    $result = $conn->query($sql);
+    
+        $row = $result->fetch_assoc();
+        return $row["name"];
+    
+}
+
+?>
 
---
+++ Hospital_Management_System/public/doctorList.php
@@ -0,0 +1,7 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php
+$username = getDoctorById(1001);
+
+echo "Username: " . $username . "<br>";
+?>
