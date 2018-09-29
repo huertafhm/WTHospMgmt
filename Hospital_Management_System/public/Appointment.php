8ce156d62db4862b8a480a0362d4487a129ea647
KaiyuanZ <kaiyuan.zheng-1@student.uts.edu.au>
2018-09-29 12:26:54 +1000

    appointment

---
+++ Hospital_Management_System/public/Appointment.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background: #b5d5e8;
+}
+h1 {font-size: 300%}
+h2 {font-size: 200%}
+form {font-size: 150%}
+</style>
+
+<body>
+<h1>Hospital Management System</h1>
+<h2>Appointment</h2>
+
+
+  Booked Slots:<br>
+  <input type="date" name="Date and time"><br>
+  Free Slots:<br>
+  <input type="date" name="Date and time"><br>
+   Date and time:<br>
+  <input type="date" name="Date and time"><br>
+  Patient's Condition:<br>
+  <input type="text" name="Visit Reason"><br>
+  <input type="Submit" value="Back">
+  
+ <ul>
+        <li><a href="patientDashboard.php">Patient Dashboard</a>
+    </li>
+  </ul>
+  
+
+
+
+     <footer>
+   &copy;<?php echo date('Y');?> Hospital
+   </footer>
+</body>
+
+
+
+</html>
+
