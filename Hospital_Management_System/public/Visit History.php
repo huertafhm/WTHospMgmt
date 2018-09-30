b6019e210fd4d14aa19937c4ecd5e1fa7f708c04
zhuoyanglai <42957365+zhuoyanglai@users.noreply.github.com>
2018-09-30 16:00:19 +1000

    Add files via upload

---
+++ Hospital_Management_System/public/Visit History.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8"> 
+<title></title> 
+</head>
+<style>
+body {
+  background: #b5d5e8;
+}
+h1 {font-size: 200%}
+h2 {font-size: 100%}
+.td{height:50px;} 
+  #i1 {
+            text-align: center;
+            color: #0c040a;
+            font-size:larger;
+            width: 100px;
+            height: 30px;
+            }
+
+</style>
+<h1 align="center">Hospital Management System</h1>
+<h2 align="center">Visit History</h2>
+<body>
+
+<table border="1" align="center" >
+  <caption></caption>
+  <tr>
+    <th>date and time</th>
+    <th>doctors'name</th>
+    <th>visit reason</th>
+    <th>doctors'suggestion</th>
+  </tr>
+  <tr>
+    <td class="td"></td>
+    <td class="td"></td>
+    <td class="td"></td>
+    <td class="td"></td>
+  </tr>
+</table>
+<br>
+<div align="center">
+<input id="i1" type="Submit" value="Back">
+</div>
+</body>
+</html>
