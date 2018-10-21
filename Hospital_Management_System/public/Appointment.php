8ce3530f3ea080cf920e7a4dfdd93affd6ac8db5
KaiyuanZ <kaiyuan.zheng-1@student.uts.edu.au>
2018-10-21 14:48:57 +1100

    Submit the change

--- Hospital_Management_System/public/Appointment.php
+++ Hospital_Management_System/public/Appointment.php
@@ -1,44 +1,72 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-body {
-  background: #b5d5e8;
-}
-h1 {font-size: 300%}
-h2 {font-size: 200%}
-form {font-size: 150%}
-</style>
+<meta charset="utf-8" />
+<title>Hospibal appointment</title>
+<link rel="stylesheet" href="css/index.css">
 
+</head>
 <body>
-<h1>Hospital Management System</h1>
-<h2>Appointment</h2>
+
+<div class="header">
+<div class="logo">
+<img src="image/logogo.jpg" alt="logo">
+</div>
+<div class="nav"> 
+<ul>
+<li><a href="patientDashboard.php">Home page</a>
+<li><a href="Visit History.php">Visit History</a>
+<li><a href="Doctors List.php">Doctors' List</a>
+</ul>
+</div>
+</div>
 
 
-  Booked Slots:<br>
-  <input type="date" name="Date and time"><br>
-  Free Slots:<br>
-  <input type="date" name="Date and time"><br>
-   Date and time:<br>
+
+
+<div class="main">
+<div class="top">
+<img src="image/appointments.jpg">
+</div>
+
+<div class="topLayer">
+
+</div>
+<!-- 最上层的内容 -->
+<div class="btn">
+<p>Make Your New Appointment</p>
+<button>Plan &nbsp;&nbsp;&gt;</button>
+</div>
+
+<div class="middle"></div>
+ Select the Date and time:<br>
   <input type="date" name="Date and time"><br>
   Patient's Condition:<br>
-  <input type="text" name="Visit Reason"><br>
-  <input type="Submit" value="Back">
-  
- <ul>
-        <li><a href="patientDashboard.php">Patient Dashboard</a>
-    </li>
-  </ul>
-  
+<select>
+  <option value ="Orthopedics">Orthopedics</option>
+  <option value ="Internal Medicine">Internal Medicine</option>
+  <option value="Pediatrics">Pediatrics</option>
+  <option value="Ophthalmology">Ophthalmology</option>
+  <option value="Dentistry">Dentistry</option>
+  <option value="Others">Others</option>
+</select>
+</div>
+ 
 
 
-
-     <footer>
-   &copy;<?php echo date('Y');?> Hospital
-   </footer>
 </body>
 
 
 
-</html>
 
+<div class="bottom"></div>
+
+
+<div class="footer">
+     <footer>
+   &copy;<?php echo date('Y');?> Hospital
+   </footer>
+</div>
+</body>
+</html>
