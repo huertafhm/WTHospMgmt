f42e3e9a55419d91ee65ce468f0febff04700bb5
huertafhm <huerta.fhm@gmail.com>
2018-10-22 13:28:40 +1100

    Added regexp to register

--- Hospital_Management_System/public/GUI_SignUp.php
+++ Hospital_Management_System/public/GUI_SignUp.php
@@ -1,6 +1,14 @@
-<?php require_once('../private/initialize.php'); ?>
+<?php 
+    require_once('../private/initialize.php'); 
+    include(SHARED_PATH . '/header.php');
+    
+    $patientName = isset($_POST['name']) ? $_POST['name'] : "";
+    $patientMobile = isset($_POST['mobile']) ? $_POST['mobile'] : "";;
+    $patientEmail = isset($_POST['email']) ? $_POST['email'] : "";;
+    $patientAge = isset($_POST['age']) ? $_POST['age'] : "";;
+    $patientGender = isset($_POST['gender']) ? $_POST['gender'] : "";;
 
-<?php include(SHARED_PATH . '/header.php'); ?>
+?>
 	<div class="container">
 		<h2>Sign Up</h2>
 		<div>
@@ -9,27 +17,27 @@
     				<div class="form-group row mt-4">
             			<label class="col-form-label col-md-2">Name</label>
             			<div class="col-md-4">
-            				<input class="form-control" type="text" name="name" />
+            				<input class="form-control" type="text" name="name" value="<?php echo $patientName;?>"/>
             			</div>
             			<label class="col-form-label col-md-2">Mobile</label>
             			<div class="col-md-4">
-            				<input class="form-control" type="text" name="mobile" />
+            				<input class="form-control" type="text" name="mobile" value="<?php echo $patientMobile;?>"/>
             			</div>
     				</div>
             		<div class="form-group row">
             			<label class="col-form-label col-md-2">Email</label>
             			<div class="col-md-4">
-            				<input class="form-control" type="text" name="email" />
+            				<input class="form-control" type="text" name="email" value="<?php echo $patientEmail;?>"/>
             			</div>
             			<label class="col-form-label col-md-2">Age</label>
             			<div class="col-md-4">
-            				<input class="form-control" type="text" name="age" />
+            				<input class="form-control" type="text" name="age" value="<?php echo $patientAge;?>"/>
             			</div>
             		</div>
             		<div class="form-group row">
             			<label class="col-form-label col-md-2">Gender</label>
             			<div class="col-md-4">
-            				<select name="gender" class="form-control">
+            				<select name="gender" class="form-control" value="<?php echo $patientGender;?>">
     							<option>Male</option>
     							<option>Female</option>
     						</select>
